55faba1ff9d91449610ed3b0c1b64d4e7a422d74
Shyamin Ayesh <me@shyamin.com>
2019-09-15 18:58:59 +0530

    rename vendor directory to libs

---
+++ app/core/App.php
@@ -0,0 +1,17 @@
+<?php
+namespace Avanciro\Skel\Core;
+
+use Avanciro\Skel\Core\Route;
+
+
+/**
+ * This is where Skel start doing things upon getting
+ * new request. This will include
+ */
+class App {
+    function dispatch() {
+        echo "Avanciro\Skel\Core\App\App::dispatch()";
+    }
+}
+
+?>
