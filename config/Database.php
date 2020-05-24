ff1d291f7cde581a70212f0c5fbaa402904f5c74
Shyamin Ayesh <me@shyamin.com>
2020-05-25 00:10:04 +0530

    change port

--- config/Database.php
+++ config/Database.php
@@ -28,7 +28,7 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => 'localhost',
-            'port'      => 3306,
+            'port'      => '3306',
             'database'  => 'test',
             'username'  => 'test',
             'password'  => 'test',
