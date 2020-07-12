dac1c83fdeacf80cad012601362f355ee6a6d9ac
Shyamin Ayesh <me@shyamin.com>
2020-07-12 09:04:28 +0530

    load configuration file statically

--- app/Core/Config.php
+++ app/Core/Config.php
@@ -1,12 +1,24 @@
 <?php
 
 namespace Avanciro\Skel\Core;
-use Avanciro\Skel\Core\Registry;
 
 class Config {
     public function __get($key) {
         if ( file_exists(dirname(dirname(__DIR__)).'/config/'.ucfirst($key).'.php') ):
             return (Object)(include dirname(dirname(__DIR__)).'/config/'.ucfirst($key).'.php');
+        else:
+            throw new \Exception("Configuration file not found");
+        endif;
+    }
+
+
+    /**
+     * Static method to return configuration file by
+     * just checking if it's exists or not.
+     */
+    public static function get($file) {
+        if ( file_exists(dirname(dirname(__DIR__)).'/config/'.ucfirst($file).'.php') ):
+            return (Object)(include dirname(dirname(__DIR__)).'/config/'.ucfirst($file).'.php');
         else:
             throw new \Exception("Configuration file not found");
         endif;
